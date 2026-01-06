122bbc139e7b4e460e01ac9b352f96285481b949
himanshuptl01 <himanshuptl012@gmail.com>
2026-01-06 15:08:10 +0000

    let var const ki kahani

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 14456
+let accountEmail = "himanshu19@gmail.com"
+var accountPassword ="123456"
+accountcity = "jaipur"
+
+// accountId = 2134
+
+ accountEmail = "himanshu0@gmail.com"
+ accountPassword ="0987"
+accountcity = "indore"
+
+//  console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountcity])
+
+
+
+
