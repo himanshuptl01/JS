22fe70760ec6c9fb4a12fdb8b9c69f2bf8496881
himanshuptl01 <himanshuptl012@gmail.com>
2026-01-07 06:37:24 +0000

    data type conversion

---
+++ 01_basics/02_variable.js
@@ -0,0 +1,25 @@
+"use strict"  //use all new versions
+// alert (3+3) //
+//output = error (kyu?) kyuki it run on node js and if we run in browser output=6 because browser is 
+//a js engine //
+
+//ECMA : they are js specifications resources link: https://tc39.es/ecma 262/
+
+//#data types //
+
+let name = "hitesh"  // string 
+let age = 18   // integer
+let isloggedIn = false // boolean 
+let state; //undefined
+let salary= null
+
+// there is a specific range of a numbers i.e 2 ki power 53 a
+// for big number we use bigint 
+// boolean = single or double quotes 
+// null= standalone value
+//undefined = not defined 
+// ssymbol = uniqueness 
+
+//object //
+
+console.log(typeof salary);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// lecture05 : tyoe convesion operation 
+
+// let score = 33
+
+// console.log(typeof score)
+// // console.log((typeof score));  // output = number
+
+//  let score = "33"
+//  console.log(typeof score)
+// ab manlo ki supose humko pata h ki humara opertion humko number me hi perform krna h 
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber)
+//   console.log(valueInNumber)
+
+  //33 to "33"
+  //"33abs" to NaN
+  // true to 1 and false to 0
+
+  let active = "hitesh"
+
+  let isactive = Boolean(active);
+  console.log(isactive)
+
+
